683409c4446aaccd2a773e56bdebdba239be2539
yismith <130582579+yismith@users.noreply.github.com>
2024-05-26 17:56:18 +0800

    1、增加01-书生·浦语大模型全链路开源开放体系笔记 2、增加视频AI字幕处理工具

---
+++ helper/ai_subtitle_helper.py
@@ -0,0 +1,28 @@
+import json
+import os
+
+if __name__ == '__main__':
+
+    # 课程名称
+    course_name = '01-introduction'
+
+    # 定义文件路径
+    json_file_path = os.path.join(os.path.dirname(__file__), '..', 'camp2', 'assets', 'video', 'aisubtitle',
+                                  '%s.json' % course_name)
+    txt_file_path = os.path.join(os.path.dirname(__file__), '..', 'camp2', 'assets', 'video', 'aisubtitle',
+                                 '%s.txt' % course_name)
+
+    # 读取 JSON 文件
+    with open(json_file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    # 提取并拼接 content 字段
+    content_list = [entry['content'] for entry in data['body']]
+    concatenated_content = ''.join(content_list)
+
+    # 将拼接好的内容写入 txt 文件
+    with open(txt_file_path, 'w', encoding='utf-8') as file:
+        file.write(concatenated_content)
+
+    print(f"拼接后的内容已写入到 {txt_file_path}")
+
